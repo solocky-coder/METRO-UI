8d1c5e3fb5bb2f1f562158cd945be6fbdc8bed07
PAINT711 <solocky@gmail.com>
2026-09-10 18:06:41 +0200

    Expose audio clip recording APIs

--- src/sequencer/SequencerEngine.h
+++ src/sequencer/SequencerEngine.h
@@ -17,6 +17,7 @@ struct SequencerTrackInfo
     int sliceIdx = -1; int midiChannel = 0; Sf2PresetInfo preset; int numClips = 0;
     bool isSfzInstrument = false;
     int64_t networkRouteId = 0; int32_t networkSourceId = 0; int networkSourceChannel = 0;
+    int numAudioClips = 0;
 };
 
 struct SequencerClipInfo
@@ -45,11 +46,12 @@ public:
     void addOrUpdateSfTrackOnChannel(const Sf2PresetInfo& preset, int midiChannel0Based, juce::Colour colour);
     void addSfzTrack(const juce::String& name, int midiChannel0Based, juce::Colour colour); void removeSfzTrack();
 
-    int addNetworkAudioTrack(int64_t routeId, int32_t sourceId, int sourceChannel,
-                             const juce::String& sourceName, const juce::String& userName = {});
+    int addNetworkAudioTrack(int64_t routeId, int32_t sourceId, int sourceChannel, const juce::String& sourceName, const juce::String& userName = {});
     bool setNetworkAudioTrackRoute(int trackIndex, int64_t routeId, int32_t sourceId, int sourceChannel);
     bool isNetworkAudioTrack(int trackIndex) const noexcept;
     bool getNetworkAudioRoute(int trackIndex, int64_t& routeId, int32_t& sourceId, int& sourceChannel) const noexcept;
+    bool addAudioClip(int trackIndex, const AudioClip& clip);
+    int getNumAudioClips(int trackIndex) const noexcept;
 
     int getNumClips(int trackIndex) const; SequencerClipInfo getClipInfo(int trackIndex, int clipIndex) const; MidiClip* getClip(int trackIndex, int clipIndex = 0); MidiClip& getClip();
     int addClip(int trackIndex, int64_t startTick, int64_t lengthTicks = MidiClip::kPPQ * 4 * 4); void removeClip(int trackIndex, int clipIndex); void setClipStartTick(int trackIndex, int clipIndex, int64_t newStartTick); void setClipLengthTicks(int trackIndex, int clipIndex, int64_t newLength);
@@ -66,6 +68,5 @@ public:
 private:
     struct Impl;
     std::unique_ptr<Impl> impl;
-
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SequencerEngine)
 };
